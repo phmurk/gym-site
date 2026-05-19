7ca56475b47e50045a02948f002113b52aeb7add
polina <poly.vahmurka@gmail.com>
2026-05-19 22:33:29 +0300

    navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,33 +1,185 @@
 import "./Navbar.css";
+// import logo from "../../assets/icons/logo.svg";
+// import defaultAvatar from "../../assets/icons/default-user-avatar.svg";
+import { NavLink } from "react-router-dom";
+import { useState, useEffect } from "react";
+// import bootstrap from "bootstrap";
+import { Offcanvas } from "bootstrap";
 
 function Navbar() {
+  const [showNavbar, setShowNavbar] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+      if (currentScrollY > lastScrollY && currentScrollY > 80) {
+        setShowNavbar(false);
+      } else {
+        setShowNavbar(true);
+      }
+      setLastScrollY(currentScrollY);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [lastScrollY]);
+
+  const closeOffcanvas = () => {
+    const offcanvasElement = document.getElementById("mobileMenu");
+    if (offcanvasElement) {
+      const offcanvasInstance = Offcanvas.getInstance(offcanvasElement);
+      if (offcanvasInstance) {
+        offcanvasInstance.hide();
+      }
+    }
+  };
+
   return (
-    <nav className="navbar navbar-expand-lg custom-navbar">
+    <nav
+      className={`navbar navbar-expand-lg custom-navbar ${
+        showNavbar ? "navbar-show" : "navbar-hide"
+      }`}
+    >
       <div className="container">
-        <div className="logo">
-          <a className="navbar-brand logo" href="/">
+        <NavLink className="navbar-brand logo" to="/">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="34"
+            height="34"
+            fill="currentColor"
+            className="bi bi-fingerprint logo-icon"
+            viewBox="0 0 16 16"
+          >
+            <path d="M8.06 6.5a.5.5 0 0 1 .5.5v.776a11.5 11.5 0 0 1-.552 3.519l-1.331 4.14a.5.5 0 0 1-.952-.305l1.33-4.141a10.5 10.5 0 0 0 .504-3.213V7a.5.5 0 0 1 .5-.5Z" />
+            <path d="M6.06 7a2 2 0 1 1 4 0 .5.5 0 1 1-1 0 1 1 0 1 0-2 0v.332q0 .613-.066 1.221A.5.5 0 0 1 6 8.447q.06-.555.06-1.115zm3.509 1a.5.5 0 0 1 .487.513 11.5 11.5 0 0 1-.587 3.339l-1.266 3.8a.5.5 0 0 1-.949-.317l1.267-3.8a10.5 10.5 0 0 0 .535-3.048A.5.5 0 0 1 9.569 8m-3.356 2.115a.5.5 0 0 1 .33.626L5.24 14.939a.5.5 0 1 1-.955-.296l1.303-4.199a.5.5 0 0 1 .625-.329" />
+            <path d="M4.759 5.833A3.501 3.501 0 0 1 11.559 7a.5.5 0 0 1-1 0 2.5 2.5 0 0 0-4.857-.833.5.5 0 1 1-.943-.334m.3 1.67a.5.5 0 0 1 .449.546 10.7 10.7 0 0 1-.4 2.031l-1.222 4.072a.5.5 0 1 1-.958-.287L4.15 9.793a9.7 9.7 0 0 0 .363-1.842.5.5 0 0 1 .546-.449Zm6 .647a.5.5 0 0 1 .5.5c0 1.28-.213 2.552-.632 3.762l-1.09 3.145a.5.5 0 0 1-.944-.327l1.089-3.145c.382-1.105.578-2.266.578-3.435a.5.5 0 0 1 .5-.5Z" />
+            <path d="M3.902 4.222a5 5 0 0 1 5.202-2.113.5.5 0 0 1-.208.979 4 4 0 0 0-4.163 1.69.5.5 0 0 1-.831-.556m6.72-.955a.5.5 0 0 1 .705-.052A4.99 4.99 0 0 1 13.059 7v1.5a.5.5 0 1 1-1 0V7a3.99 3.99 0 0 0-1.386-3.028.5.5 0 0 1-.051-.705M3.68 5.842a.5.5 0 0 1 .422.568q-.044.289-.044.59c0 .71-.1 1.417-.298 2.1l-1.14 3.923a.5.5 0 1 1-.96-.279L2.8 8.821A6.5 6.5 0 0 0 3.058 7q0-.375.054-.736a.5.5 0 0 1 .568-.422m8.882 3.66a.5.5 0 0 1 .456.54c-.084 1-.298 1.986-.64 2.934l-.744 2.068a.5.5 0 0 1-.941-.338l.745-2.07a10.5 10.5 0 0 0 .584-2.678.5.5 0 0 1 .54-.456" />
+            <path d="M4.81 1.37A6.5 6.5 0 0 1 14.56 7a.5.5 0 1 1-1 0 5.5 5.5 0 0 0-8.25-4.765.5.5 0 0 1-.5-.865m-.89 1.257a.5.5 0 0 1 .04.706A5.48 5.48 0 0 0 2.56 7a.5.5 0 0 1-1 0c0-1.664.626-3.184 1.655-4.333a.5.5 0 0 1 .706-.04ZM1.915 8.02a.5.5 0 0 1 .346.616l-.779 2.767a.5.5 0 1 1-.962-.27l.778-2.767a.5.5 0 0 1 .617-.346m12.15.481a.5.5 0 0 1 .49.51c-.03 1.499-.161 3.025-.727 4.533l-.07.187a.5.5 0 0 1-.936-.351l.07-.187c.506-1.35.634-2.74.663-4.202a.5.5 0 0 1 .51-.49" />
+          </svg>
+          <span>FitPulse</span>
+        </NavLink>
+
+        <button
+          className="navbar-toggler custom-toggler"
+          type="button"
+          data-bs-toggle="offcanvas"
+          data-bs-target="#mobileMenu"
+          aria-controls="mobileMenu"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        {/*  для ноутов */}
+        <div className="desktop-menu">
+          <ul className="navbar-nav nav-links">
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/">
+                Главная
+              </NavLink>
+            </li>
+
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/schedule">
+                Расписание
+              </NavLink>
+            </li>
+
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/prices">
+                Цены
+              </NavLink>
+            </li>
+
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/trainers">
+                Тренеры
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+        <div className="profile-wrapper">
+          <NavLink className="profile-link" to="/profile">
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              width={28}
-              height={28}
+              width="30"
+              height="30"
               fill="currentColor"
-              viewBox="0 0 24 24"
-              className="logo-icon"
+              className="bi bi-person-circle"
+              viewBox="0 0 16 16"
             >
-              <path d="M16.91 2.29a.996.996 0 0 0-1.41 0l-1.59 1.59-1.09-1.09a.996.996 0 0 0-1.41 0l-2.3 2.3c-.19.19-.29.44-.29.71s.11.52.29.71L12.6 10l-2.59 2.59L6.52 9.1a.996.996 0 0 0-1.41 0l-2.3 2.3a.996.996 0 0 0 0 1.41L3.9 13.9l-1.59 1.59a.996.996 0 0 0 0 1.41l4.8 4.8c.2.2.45.29.71.29s.51-.1.71-.29l1.59-1.59 1.09 1.09c.2.2.45.29.71.29s.51-.1.71-.29l2.3-2.3c.19-.19.29-.44.29-.71s-.11-.52-.29-.71l-3.49-3.49 2.59-2.59 3.49 3.49c.2.2.45.29.71.29s.51-.1.71-.29l2.3-2.3a.996.996 0 0 0 0-1.41l-1.09-1.09 1.59-1.59a.996.996 0 0 0 0-1.41l-4.8-4.8ZM7.8 19.59 4.41 16.2l.89-.89 3.39 3.39zm4.1-.5L4.91 12.1l.89-.89 6.99 6.99zm6.3-6.3L11.21 5.8l.89-.89 6.99 6.99zm-2-8.37 3.39 3.39-.89.89-3.39-3.39z"></path>
+              <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
+              <path
+                fill-rule="evenodd"
+                d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"
+              />
             </svg>
-
-            <span>GYMFIT</span>
-          </a>
+            <span>Профиль</span>
+          </NavLink>
         </div>
       </div>
-
-      <div className="nav-links">
-        <a href="/">Home</a>
-        <a href="/trainers">Trainers</a>
-        <a href="/prices">Prices</a>
-        <a href="/schedule">Schedule</a>
-        <a href="/profile">Profile</a>
+      {/* для телефонов */}
+      <div
+        className="offcanvas offcanvas-end mobile-offcanvas"
+        tabIndex={-1}
+        id="mobileMenu"
+        aria-labelledby="mobileMenuLabel"
+      >
+        <div className="offcanvas-header">
+          <h5 className="offcanvas-title">FitPulse</h5>
+          <button
+            type="button"
+            className="btn-close btn-close-white"
+            data-bs-dismiss="offcanvas"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div className="offcanvas-body">
+          <ul className="navbar-nav mobile-links">
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/" onClick={closeOffcanvas}>
+                Главная
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                className="nav-link"
+                to="/schedule"
+                onClick={closeOffcanvas}
+              >
+                Расписание
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                className="nav-link"
+                to="/prices"
+                onClick={closeOffcanvas}
+              >
+                Цены
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                className="nav-link"
+                to="/trainers"
+                onClick={closeOffcanvas}
+              >
+                Тренеры
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                className="nav-link"
+                to="/profile"
+                onClick={closeOffcanvas}
+              >
+                Профиль
+              </NavLink>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
