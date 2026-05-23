6a664b4b31776d2b4356c8b859365976368736f8
polina <poly.vahmurka@gmail.com>
2026-05-24 00:42:25 +0300

    creatr FAQ

---
+++ src/components/Home/OurMission/OurMission.tsx
@@ -0,0 +1,14 @@
+import "./OurMission.css";
+
+// import { motion } from "framer-motion";
+import "./OurMission.css";
+
+function OurMission() {
+  return (
+    <section className="our-mission">
+      <div className="container"></div>
+    </section>
+  );
+}
+
+export default OurMission;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,13 @@
 import Hero from "../../components/Home/Hero/Hero";
+import OurMission from "../../components/Home/OurMission/OurMission";
+import Questions from "../../components/Home/Questions/Questions";
 
 function Home() {
   return (
     <>
       <Hero />
+      <OurMission />
+      <Questions />
     </>
   );
 }
